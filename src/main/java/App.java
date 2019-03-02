2503dc7ade6dd3982269e6ed5d416257fd50b4e4
gaborKriger <regirck@gmail.com>
2019-03-02 15:01:17 +0100

    create basic server

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,34 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("It is work!");
+    public static void main(String[] args) throws IOException {
+
+        // create server and set port
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        // create route and collect the context
+        server.createContext("/test", new MyHandler());
+        // creates a default executor
+        server.setExecutor(null);
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            String response = "This is the response";
+            // create http header (set the length)
+            httpExchange.sendResponseHeaders(200, response.length());
+            // stream inside the date
+            OutputStream os = httpExchange.getResponseBody();
+            // convert the data
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
